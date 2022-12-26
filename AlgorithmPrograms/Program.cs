126479376b2eff181f782a3aba2282bad34d02bc
shubham07h <shubham1609sharma@gmail.com>
2022-12-26 23:39:34 +0530

    commit

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class InsertionSort
+{
+    public static void Main(string[] args)
+    {
+        string[] words = Console.ReadLine().Split(' ');
+
+        // Insertion Sort Algorithm
+        for (int i = 1; i < words.Length; i++)
+        {
+            string key = words[i];
+            int j = i - 1;
+
+            while (j >= 0 && words[j].CompareTo(key) > 0)
+            {
+                words[j + 1] = words[j];
+                j = j - 1;
+            }
+
+            words[j + 1] = key;
+        }
+
+        // Print the sorted list
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
